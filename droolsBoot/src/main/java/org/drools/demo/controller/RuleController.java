44dcc539816ddaadaeb6dae4d1685597a7110dba
zzl <coderzzl@163.com>
2022-04-11 17:36:58 +0800

    drools with springboot 7.66.0

--- droolsBoot/src/main/java/org/drools/demo/controller/RuleController.java
+++ droolsBoot/src/main/java/org/drools/demo/controller/RuleController.java
@@ -3,9 +3,10 @@ package org.drools.demo.controller;
 import org.drools.demo.service.RuleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
 
 /**
  * @author Julyan
@@ -20,8 +21,10 @@ public class RuleController {
     @Autowired
     private RuleService ruleService;
 
-    @RequestMapping("/simpleRule")
-    public String simpleRule(ModelAndView modelAndView) {
+    @RequestMapping(value = "/simpleRule",method = RequestMethod.POST)
+    @ResponseBody
+    public String simpleRule(@RequestBody Map<String,String> map) {
+        System.out.println(map);
         ruleService.rule();
         return "index";
     }
