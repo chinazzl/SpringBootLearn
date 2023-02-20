82a95e1fc8c12932bab6df13087b7ae97d133c28
zzl <coderzzl@163.com>
2023-02-20 18:14:00 +0800

    web Base Template

--- droolsBoot/src/main/java/org/drools/demo/controller/BaelineController.java
+++ droolsBoot/src/main/java/org/drools/demo/controller/BaelineController.java
@@ -7,6 +7,7 @@ import org.drools.demo.service.IBaselineService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -43,8 +44,8 @@ public class BaelineController {
      */
     @RequestMapping("/getbaseline")
     @ResponseBody
-    public Result<BaselineBO> getbaseline() {
-        String ip = "102.200.218.193";
+    public Result<BaselineBO> getbaseline(@RequestParam("ip") String ip) {
+        // String ip = "102.200.218.193";
         BaselineBO cpuBaseline = baselineService.getCpuBaseline(ip);
 
         return Result.ok(cpuBaseline);
